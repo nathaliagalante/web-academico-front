8f0f3556f7866bd90cc8358f79b474a40496af67
Nathalia Galante <nathalia.g.a@hotmail.com>
2021-08-31 11:57:45 -0300

    improvement: new table from react-table

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,29 +1,72 @@
 import React from 'react';
 import { Table } from 'react-bootstrap'
+import { useTable } from 'react-table'
 
-function TableComponent(props){
+function TableComponent({columns, data}){
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({
+        columns,
+        data,
+      });
+
     return(
-        <Table responsive bordered>
+        <Table responsive bordered {...getTableProps()}>
             <thead>
-                <tr>
-                    {props.th && props.th.map(item => {
-                        return <th scope="col" className="text-center">{item}</th>
-                    })}
-                </tr>
-             </thead>
-            <tbody>
-                {props.items && props.items.map(item => {
-                return <tr key={item.id}>
-                    <th className="text-center" scope="row">{item.periodo}</th>
-                    <td className="text-center">{item.semestre}</td>
-                    <td>{item.nome}</td>
-                    <td>{item.professor}</td>
-                    <td className="text-center">{item.nota}</td>
-                    <td className="text-center">{item.situacao}</td>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th className="text-center" {...column.getHeaderProps()}>
+                                {column.render('Header')}
+                            </th>
+                        ))}
                     </tr>
+                ))}
+            </thead>
+
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row)
+                    return(
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>
+                                    {cell.render('Cell')}
+                                </td>
+                            })}
+                        </tr>
+                    )
                 })}
             </tbody>
-        </Table>  
+        </Table>
+        // <Table responsive bordered>
+        //     <thead>
+        //         <tr>
+        //             {props.th && props.th.map(item => {
+        //                 return <th scope="col" className="text-center">{item}</th>
+        //             })}
+        //         </tr>
+        //      </thead>
+        //     <tbody>
+        //         {/* {props.th && props.th.map(th => {
+                    
+        //         })} */}
+        //         {props.items && props.items.map(item => {
+        //         return <tr key={item.id}>
+        //             <th className="text-center" scope="row">{item.periodo}</th>
+        //             <td className="text-center">{item.semestre}</td>
+        //             <td>{item.nome}</td>
+        //             <td>{item.professor}</td>
+        //             <td className="text-center">{item.nota}</td>
+        //             <td className="text-center">{item.situacao}</td>
+        //             </tr>
+        //         })}
+        //     </tbody>
+        // </Table>  
     )
 }
 
