ce389969b38f7df251859dcdb7c4ea21100f6d08
Nathalia Galante <nathalia.g.a@hotmail.com>
2021-09-15 09:05:52 -0300

    feat: ACG page

--- src/pages/VidaAcademica/acg/ConsultarACG.js
+++ src/pages/VidaAcademica/acg/ConsultarACG.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, {useState, useMemo, useEffect} from 'react';
+import { Container, Button } from 'react-bootstrap';
+
+import TableComponent from '../../../components/table';
 
 const ConsultarACG = () => {
+    const [acgs, setAcgs] = useState([]);
+
+    const columns = useMemo(() => [
+        {
+            Header: 'Categoria',
+            accessor: 'categoria'
+        },
+        {
+            Header: 'Horas',
+            accessor: 'horas',
+        },
+        {
+            Header: '',
+            accessor: 'detalhes',
+            Cell: props => <Button variant="info">Detalhes</Button>,
+        }
+    ], []);
+
+    useEffect(() => {
+        const url = window.servidor + '/acgs';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAcgs(data));
+    }, []);
+
     return (
-        <div>
-            <Container>
-                <h1>Consultar ACG</h1>
-            </Container>
-        </div>
+        <Container className="">
+            <h1>ACGs</h1>
+            <hr></hr>
+            <TableComponent columns={columns} data={acgs} />
+        </Container>
+    
     )
 }
 
