0ba8a64567adb07c70636580aa33fc8f951ebc9f
Nathalia Galante <nathalia.g.a@hotmail.com>
2021-09-22 22:25:03 -0300

    improvement: icon on button 'sair'

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 import './Menu.css'
 import { Navbar, NavDropdown, Container, Nav } from 'react-bootstrap'
+import { HomeOutlined, FileOutlined, UserOutlined, VerticalLeftOutlined } from '@ant-design/icons';
 
 export const Menu = () => {
+    //assigning location variable
+    const location = useLocation();
+
+    //destructuring pathname from location
+    const { pathname } = location;
+
+    //Javascript split method to get the name of the path in array
+    const splitLocation = pathname.split("/");
+
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" fixed="top" bg="dark" variant="dark">
@@ -12,29 +23,44 @@ export const Menu = () => {
 
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto">
-                            <Nav.Link className="active" aria-current="page" href="/home">
-                                <i className="bi bi-house"></i>
+                            <Nav.Link href="/home" className={splitLocation[1] === "" ? "active" : ""}>
+                                <HomeOutlined />
                                 <span>Página Inicial</span>
-                                </Nav.Link>
+                            </Nav.Link>
                             
-                            <NavDropdown title="Vida Acadêmica" id="collasible-nav-dropdown">
+                            <NavDropdown 
+                                title={ <span> <FileOutlined /> <span>Vida Acadêmica</span> </span> } 
+                                id="collasible-nav-dropdown"
+                                className={splitLocation[1] === "" ? "active" : ""}
+                                >
+
                                 <NavDropdown.Item href="/VidaAcademica/FazerMatricula">Fazer Matrícula</NavDropdown.Item>
                                 <NavDropdown.Item href="/VidaAcademica/Horario">Consultar Horário</NavDropdown.Item>
                                 <NavDropdown.Item href="/VidaAcademica/Boletim">Consultar Boletim</NavDropdown.Item>
                                 <NavDropdown.Item href="/VidaAcademica/Historico">Consultar Histórico</NavDropdown.Item>
-                                <NavDropdown.Item href="/VidaAcademica/CancelarInscricao">Cancelar Inscrição</NavDropdown.Item>
                                 <NavDropdown.Item href="/VidaAcademica/ConsultarACG">Consultar ACG</NavDropdown.Item>
+                                <NavDropdown.Item href="/VidaAcademica/CancelarInscricao">Cancelar Inscrição</NavDropdown.Item>
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="/VidaAcademica/ResponderCPA">Responder CPA</NavDropdown.Item>
                             </NavDropdown>
+                            
+                            <NavDropdown 
+                                title={ <span> <UserOutlined /> <span>Conta</span> </span> } 
+                                id="collasible-nav-dropdown"
+                                className={splitLocation[1] === "" ? "active" : ""}
+                                >
 
-                            <NavDropdown title="Conta" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="/conta/AtualizarCadastro">Atualizar Cadastro</NavDropdown.Item>
                                 <NavDropdown.Item href="/conta/AlterarSenha">Alterar Senha</NavDropdown.Item>
                             </NavDropdown>
+                            
                         </Nav>
 
                         <Nav>
-                            <Nav.Link href="/login" className="text-white">Sair</Nav.Link>
+                            <Nav.Link href="/login" className="text-white">
+                                <span>Sair</span>
+                                <VerticalLeftOutlined />
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
